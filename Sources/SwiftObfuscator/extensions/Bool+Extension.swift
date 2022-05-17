1b2f4e67587e2e9320a314fdf0bb2fa60ca98e05
tomieq <tomieq@wp.pl>
2022-05-17 14:47:59 +0200

    Added Bool extension

---
+++ Sources/SwiftObfuscator/extensions/Bool+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Bool+Extension.swift
+//
+//
+//  Created by Tomasz Kucharski on 17/05/2022.
+//
+
+import Foundation
+
+extension Bool {
+    var not: Bool {
+        return !self
+    }
+}
