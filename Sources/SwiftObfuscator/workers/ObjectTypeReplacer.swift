8a515857f920e2b8d0628208dfd8816bef990df0
tomieq <tomieq@wp.pl>
2022-05-17 20:48:01 +0200

    Implemented ObjectTypeReplacer

---
+++ Sources/SwiftObfuscator/workers/ObjectTypeReplacer.swift
@@ -0,0 +1,20 @@
+//
+//  ObjectTypeReplacer.swift
+//
+//
+//  Created by Tomasz Kucharski on 17/05/2022.
+//
+
+import Foundation
+
+struct ObjectTypeReplacer {
+    static func replace(_ type: ProjectObjectType, with name: String, in fileContent: String) -> String {
+        guard let regex = try? NSRegularExpression(pattern: "\\b\(type.name)\\b") else {
+            return fileContent
+        }
+        let mutableString = NSMutableString(string: fileContent)
+        let range = NSRange(location: 0, length: fileContent.utf16.count)
+        regex.replaceMatches(in: mutableString, options: [], range: range, withTemplate: name)
+        return mutableString as String
+    }
+}
---
+++ Tests/ObjectTypeReplacerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ObjectTypeReplacerTests.swift
+//
+//
+//  Created by Tomasz Kucharski on 17/05/2022.
+//
+
+import Foundation
+import XCTest
+@testable import SwiftObfuscator
+
+class ObjectTypeReplacerTests: XCTestCase {
+    func testSimpleClassName() {
+        let content = "final class User {}"
+        let type = ProjectObjectType(swiftObjectType: .class, name: "User")
+        let replaced = ObjectTypeReplacer.replace(type, with: "Object1", in: content)
+        XCTAssertEqual(replaced, "final class Object1 {}")
+    }
+
+    func testClassNameWithSuffix() {
+        let content = """
+            final class User {}
+            class UserData {}
+        """
+        let type = ProjectObjectType(swiftObjectType: .class, name: "User")
+        let replaced = ObjectTypeReplacer.replace(type, with: "Object1", in: content)
+
+        let expected = """
+            final class Object1 {}
+            class UserData {}
+        """
+        XCTAssertEqual(replaced, expected)
+    }
+}
