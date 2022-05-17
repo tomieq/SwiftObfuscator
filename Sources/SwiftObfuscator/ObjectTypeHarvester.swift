f1c31d649079acf055cb4dfbe4a98858c7b4e094
tomieq <tomieq@wp.pl>
2022-05-17 20:22:12 +0200

    Implemented ObjectTypeHarvester

---
+++ Sources/SwiftObfuscator/ObjectTypeHarvester.swift
@@ -0,0 +1,40 @@
+//
+//  ObjectTypeHarvester.swift
+//
+//
+//  Created by Tomasz Kucharski on 17/05/2022.
+//
+
+import Foundation
+
+enum SwiftObjectType: String, CaseIterable {
+    case `class`
+    case `enum`
+    case `struct`
+}
+
+struct ProjectObjectType: Equatable {
+    let swiftObjectType: SwiftObjectType
+    let name: String
+}
+
+class ObjectTypeHarvester {
+    static func getObjectTypes(fileContent txt: String) -> [ProjectObjectType] {
+        var foundObjectTypes: [ProjectObjectType] = []
+        let range = NSRange(location: 0, length: txt.utf16.count)
+
+        for type in SwiftObjectType.allCases {
+            let rawType = type.rawValue
+            guard let regex = try? NSRegularExpression(pattern: "\\b\(rawType)\\s([a-zA-Z0-9_]+)") else {
+                continue
+            }
+            for result in regex.matches(in: txt, options: [], range: range) {
+                let matchingString = (txt as NSString).substring(with: result.range)
+                let name = matchingString[rawType.count...matchingString.count - 1].trimmingCharacters(in: .whitespaces)
+                foundObjectTypes.append(ProjectObjectType(swiftObjectType: type, name: name))
+            }
+        }
+
+        return foundObjectTypes
+    }
+}
---
+++ Tests/ObjectTypeHarvesterTests.swift
@@ -0,0 +1,68 @@
+//
+//  ObjectTypeHarvesterTests.swift
+//
+//
+//  Created by Tomasz Kucharski on 17/05/2022.
+//
+
+import Foundation
+import XCTest
+@testable import SwiftObfuscator
+
+class ObjectTypeHarvesterTests: XCTestCase {
+    func testFindClassDefinition() {
+        let content = """
+            class User: Human {}
+            class Dog {}
+        """
+        let objectTypes = ObjectTypeHarvester.getObjectTypes(fileContent: content)
+        XCTAssertEqual(objectTypes[safeIndex: 0]?.swiftObjectType, .class)
+        XCTAssertEqual(objectTypes[safeIndex: 0]?.name, "User")
+        XCTAssertEqual(objectTypes[safeIndex: 1]?.swiftObjectType, .class)
+        XCTAssertEqual(objectTypes[safeIndex: 1]?.name, "Dog")
+    }
+
+    func testFindStructDefinition() {
+        let content = """
+            struct Profile{}
+            struct DefaultKeys {}
+        """
+        let objectTypes = ObjectTypeHarvester.getObjectTypes(fileContent: content)
+        XCTAssertEqual(objectTypes[safeIndex: 0]?.swiftObjectType, .struct)
+        XCTAssertEqual(objectTypes[safeIndex: 0]?.name, "Profile")
+        XCTAssertEqual(objectTypes[safeIndex: 1]?.swiftObjectType, .struct)
+        XCTAssertEqual(objectTypes[safeIndex: 1]?.name, "DefaultKeys")
+    }
+
+    func testFindEnumDefinition() {
+        let content = """
+            enum Size {
+                case small
+                case large
+            }
+        """
+        let objectTypes = ObjectTypeHarvester.getObjectTypes(fileContent: content)
+        XCTAssertEqual(objectTypes[safeIndex: 0]?.swiftObjectType, .enum)
+        XCTAssertEqual(objectTypes[safeIndex: 0]?.name, "Size")
+    }
+
+    func testFindMixedDefinitions() {
+        let content = """
+            enum Alignment {
+                case left
+                case center
+            }
+            class View {
+                var alignment: Alignment
+            }
+            struct Title {
+                let label: String
+            }
+        """
+        let objectTypes = ObjectTypeHarvester.getObjectTypes(fileContent: content)
+        XCTAssertEqual(objectTypes.count, 3)
+        XCTAssertTrue(objectTypes.contains(ProjectObjectType(swiftObjectType: .enum, name: "Alignment")))
+        XCTAssertTrue(objectTypes.contains(ProjectObjectType(swiftObjectType: .class, name: "View")))
+        XCTAssertTrue(objectTypes.contains(ProjectObjectType(swiftObjectType: .struct, name: "Title")))
+    }
+}
